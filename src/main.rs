bbfd1d2b46c121e7216fd34d6783ac470b3f42f1
spcmnd <spcmndd@protonmail.com>
2022-06-10 23:04:04 +0200

    Add sqlite connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use sqlite;
+
+fn list(connection: sqlite::Connection) {
+    println!("List action");
+    connection
+        .iterate("SELECT * FROM todos", |todos| {
+            println!("{:?}", todos);
+            true
+        })
+        .unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let action = std::env::args().nth(1).expect("No action provided");
+    let connection = sqlite::open("todo.db").unwrap();
+    connection
+        .execute(
+            "
+        CREATE TABLE IF NOT EXISTS todos (id INTEGER, name TEXT);
+        INSERT INTO todos VALUES (1, 'First');
+        INSERT INTO todos VALUES (2, 'Second');
+        ",
+        )
+        .unwrap();
+
+    match action.as_str() {
+        "list" => list(connection),
+        _ => println!("Wrong action"),
+    };
 }
